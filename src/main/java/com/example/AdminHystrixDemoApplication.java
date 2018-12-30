ff7da7a084e6d9b4a908ef8cdaec39c63c45a07e
sachinrokade <srokade918@gmail.com>
2018-12-29 22:51:34 -0800

    jar 2 war

--- src/main/java/com/example/AdminHystrixDemoApplication.java
+++ src/main/java/com/example/AdminHystrixDemoApplication.java
@@ -19,7 +19,7 @@ public class AdminHystrixDemoApplication extends SpringBootServletInitializer {
     }
 
 	public static void main(String[] args) {
-		System.out.println("converterted jar 2 war");
+		System.out.println("converted jar 2 war");
 		SpringApplication.run(AdminHystrixDemoApplication.class, args);
 	}
 
